3792ca7cd8bb1070afe37399804ca6fa5cf7bff1
oliverengel <imwebunterwegs@icloud.com>
2021-03-20 13:41:46 +0000

    added view of assetSelection and asset

--- tiy/tracker/urls.py
+++ tiy/tracker/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = 'tracker'
 
 urlpatterns =[
-    path('', views.index, name='index'),		#index -> dashboard
-    path('trades', views.trades, name='trades'),	#overview of all trades, with filter
-    path('asset', views.asset, name='asset'),		#one asset: A) balance sheet, B) history of trades
+    path('', views.index, name='index'),		                        #index -> dashboard
+    path('trades', views.trades, name='trades'),	                    #overview of all trades, with filter
+    path('assetSelection',views.assetSelection, name='assetSelection'), #User has to select on asset in order to enter the assetView
+    path('asset/<str:asset_symbol>', views.asset, name='asset'),		#one asset: A) balance sheet, B) history of trades
 
     path('createTrade', views.createTrade, name='createTrade'),
 ]
--- tiy/tracker/views.py
+++ tiy/tracker/views.py
@@ -1,14 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 from django_tables2 import RequestConfig
 
-from .models import Trade
+from .models import Trade, Asset
 from .forms import TradeForm
 from .tables import TradeTable
 
 # Create your views here.
-def index(request):
+def index(request):	#Dashboard
     tradeForm = TradeForm()
     context = {'tradeForm': tradeForm}
     return render(request, 'tracker/index.html', context)
@@ -20,13 +20,27 @@ def trades(request):
     trades = Trade.objects.all()
     table = TradeTable(trades)
     RequestConfig(request).configure(table)
-    context = {'tradeForm': tradeForm, 'trades': trades, 'table': table}
+    context = {'tradeForm': tradeForm, 'table': table}
     return render(request, 'tracker/trades.html', context)
 
 
 
-def asset(request):
-    return HttpResponse('overview of one asset')
+def assetSelection(request):
+    assets = Asset.objects.all().order_by('symbol')
+    asset_types = dict(Asset.ASSET_TYPES)
+    used_types_set = {x.type for x in assets}                           #Gets all used assetTypes from the db as set
+    used_types_dict = {x: asset_types[x] for x in used_types_set}       #Converts all used assetTypes into a dict
+    context = {'assets': assets, 'used_types': used_types_dict}
+    return render(request, 'tracker/asset_selection.html', context)
+
+
+def asset(request, asset_symbol):
+    asset = Asset.objects.get(symbol=asset_symbol)
+    trades = Trade.objects.filter(asset_buy=asset) | Trade.objects.filter(asset_sell=asset)
+    table = TradeTable(trades)
+    RequestConfig(request).configure(table)
+    context = {'table': table}
+    return render(request, 'tracker/asset.html', context)
 
 
 
@@ -37,4 +51,4 @@ def createTrade(request):
     if tradeForm.is_valid():
         tradeForm.save()
 
-    return redirect('/')
+    return redirect('/trades') #HttpResponseRedirect('')
